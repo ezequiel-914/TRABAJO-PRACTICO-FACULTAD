a486b40a7e10ec5d0a51299541894410a67a13c3
Ruben Ezequiel Rojas <102380209+ezequiel-914@users.noreply.github.com>
2024-11-06 09:46:01 -0300

    js hidden

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 function slideshow(){
     let slide = document.getElementById('autos');
+    let ocultar = document.getElementById('slideshow');
+    let slideOver= ocultar.style.overflowX;
     let currenTranslateX = slide.style.transform;
     
     if (currenTranslateX=== 'translateX(-10px)') {
-        slide.style.transform = 'translateX(-400px)';}
+        slide.style.transform = 'translateX(-400px)';
+        slideOver= 'hidden';}
     else if (currenTranslateX=== 'translateX(-400px)'){
         slide.style.transform = 'translateX(-800px)';
+        slideOver= 'hidden';
     } else if (currenTranslateX=== 'translateX(-800px)'){
         slide.style.transform = 'translateX(-1200px)';
+        slideOver= 'hidden';
     } else if (currenTranslateX=== 'translateX(-1200px)'){
         slide.style.transform = 'translateX(-1600px)';
+        slideOver= 'hidden';
     } else if (currenTranslateX=== 'translateX(-1600px)'){
         slide.style.transform = 'translateX(-2000px)';
+        slideOver= 'hidden';
     } else if (currenTranslateX=== 'translateX(-2000px)'){
         slide.style.transform = 'translateX(-2200px)';
+        slideOver= 'hidden';
     }else {
         slide.style.transform = 'translateX(-10px)';
+        slideOver= 'hidden';
     }
 }
 setInterval(slideshow,3000);
